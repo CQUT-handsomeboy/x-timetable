944e0685ae74e3c3c2343691967fba2b4f160011
Hanjiang <17830831606@163.com>
2024-11-28 23:37:44 +0800

    server模式部署到netlify

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,8 @@ import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import zeabur from '@zeabur/astro-adapter/serverless';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
@@ -15,5 +17,5 @@ export default defineConfig({
     }),
   ],
   output: 'server',
-  adapter: zeabur(),
+  adapter: netlify(),
 })
